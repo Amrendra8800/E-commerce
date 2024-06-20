84525ed24cbedf17091f724441240e87c078a3d6
amrendra <amrendar68r@gmail.com>
2024-06-20 23:27:12 +0530

    vercel

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { port } = require('./configs');
-const Database = require('./data-source');
-const app = require('./app');
-const Logger = require('./core/Logger');
+const { port } = require('./src/configs');
+const Database = require('./src/data-source');
+const app = require('./src/app');
+const Logger = require('./src/core/Logger');
 
 (async () => {
     try {
